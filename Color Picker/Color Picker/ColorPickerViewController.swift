8cd773492a0b3dc469fd10161ee2dbe952a78fe8
Thomas Schultz <tjschultz@thomass-macbook-pro.local>
2018-04-19 17:37:52 -0500

    added color array and functionality to display color when selected

--- Color Picker/Color Picker/ColorPickerViewController.swift
+++ Color Picker/Color Picker/ColorPickerViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ColorPickerViewController: UIViewController {
+class ColorPickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
-    @IBOutlet weak var UILabel: UILabel!
-    @IBOutlet weak var UIPicker: UIPickerView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let colors = ["Red","Orange","Yellow","Green","Blue","Purple"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.view.backgroundColor = UIColor.red
+        label.text = "Red"
+        
         // Do any additional setup after loading the view.
     }
 
@@ -24,6 +29,50 @@ class ColorPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if colors[row] == "Red"
+        {
+            self.view.backgroundColor = UIColor.red
+            label.text = "Red"
+        }
+        if colors[row] == "Orange"
+        {
+            self.view.backgroundColor = UIColor.orange
+            label.text = "Orange"
+        }
+        if colors[row] == "Yellow"
+        {
+            self.view.backgroundColor = UIColor.yellow
+            label.text = "Yellow"
+        }
+        if colors[row] == "Green"
+        {
+            self.view.backgroundColor = UIColor.green
+            label.text = "Green"
+        }
+        if colors[row] == "Blue"
+        {
+            self.view.backgroundColor = UIColor.blue
+            label.text = "Blue"
+        }
+        if colors[row] == "Purple"
+        {
+            self.view.backgroundColor = UIColor.purple
+            label.text = "Purple"
+        }
+    }
 
     /*
     // MARK: - Navigation
