a7c075de76ba3364b2e0c796d4a13f1e43c25e12
Thomas Schultz <tjschultz@mwc-039008.dhcp.missouri.edu>
2018-04-19 13:04:20 -0500

    Added IBOutlets and constraints

---
+++ Color Picker/Color Picker/ColorPickerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ColorPickerViewController.swift
+//  Color Picker
+//
+//  Created by Thomas Schultz on 4/19/18.
+//  Copyright © 2018 array. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+
+    @IBOutlet weak var UILabel: UILabel!
+    @IBOutlet weak var UIPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
